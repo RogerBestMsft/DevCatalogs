193cf0dd1a3f57c7396b5e6d5ac391827a94857e
Roger <10090643+RogerBestMsft@users.noreply.github.com>
2023-05-30 16:50:02 -0700

    add storage to deploy

--- DevCenter/Catalogs/TFAppWithRegistry/deploy.sh
+++ DevCenter/Catalogs/TFAppWithRegistry/deploy.sh
@@ -1,3 +1,4 @@
 terraform init
-terraform plan -state=$EnvironmentState -out=$EnvironmentPlan -var-file="$EnvironmentVars" -var "resource_group_name=TestRG"
+terraform plan -state=$EnvironmentState -out=$EnvironmentPlan -var-file="$EnvironmentVars" -var "resource_group_name=$ENVIRONMENT_RESOURCE_GROUP_NAME"
 terraform apply -auto-approve -state=$EnvironmentState $EnvironmentPlan
+az storage account create --name billybob --resource-group $ENVIRONMENT_RESOURCE_GROUP_NAME --location eastus --dns-endpoint-type AzureDnsZone
