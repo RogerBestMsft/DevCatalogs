790bc32a59561c34403820e899dec679122830df
Roger <10090643+RogerBestMsft@users.noreply.github.com>
2023-06-05 16:57:39 -0700

    change output

--- DevCenter/Catalogs/SonarQubeNoSql/output.tf
+++ DevCenter/Catalogs/SonarQubeNoSql/output.tf
@@ -1,23 +1,4 @@
-output "application_id" {
-  value = azuread_application.app.application_id
-  description = "Azure AD application id"
-}
-output "application_object_id" {
-  value = azuread_application.app.object_id
-  description = "Azure AD application object id"
-}
-
-output "service_principal_name" {
-  value = azuread_service_principal.service.display_name
-  description = "Service Principal display name"
-}
-
-output "service_principal_id" {
-  value = azuread_service_principal.service.id
-  description = "Service Principal unique ID"
-}
-
-output "service_principal_secret" {
-  value = random_password.password.result
-  description = "Azure AD Service Principal secret"
+output "sitenameoutput" {
+  value = var.siteName
+  description = "Web app site name"
 }
