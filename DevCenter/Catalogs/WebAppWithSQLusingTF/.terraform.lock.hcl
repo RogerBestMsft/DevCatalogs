bdde654cdc6c658d1b286077757e2ae49df59738
Roger <10090643+RogerBestMsft@users.noreply.github.com>
2023-08-29 11:11:23 -0700

    Update access and catalog item

---
+++ DevCenter/Catalogs/WebAppWithSQLusingTF/.terraform.lock.hcl
@@ -0,0 +1,82 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/azure/azapi" {
+  version     = "1.2.0"
+  constraints = "~> 1.2.0"
+  hashes = [
+    "h1:Z1TulSv254eRYPOI56VfrexVr7WjTEey198gOD+3o8k=",
+    "zh:0dcca6970347a67c2192be251ea1e98b5df50a8f7cb352adbf2fbdb6cbe02b03",
+    "zh:306545eedf8a6dbc64d34fdc9fe104b874d45f9ab5ca1232e9670e36376f7d04",
+    "zh:31484f398b08b10b86af2d89c89ae13e513bf738dfe90e2ff3dc564332b8c180",
+    "zh:36fda73c39c56495bfda7fa746863357ab8284d27724d809130c03fada62301f",
+    "zh:40075df5e753032bd9a7d2698e762d355524f56a4a7e8df65489d44273aa0e32",
+    "zh:6b7667b74eaa0e0884423704e48bb3468e465fd99de108e64b93c9e26c8c4b0d",
+    "zh:8248289b744cdeb9d3e20dedd13a66ba3225f8efd6694a556f69acf16825f45f",
+    "zh:86d864576520952d74b0ac5d92ed9efe09894c5405cbadddf1d95c2b39c4a514",
+    "zh:9b5d3c6a753cc57be31ba6a6e14b571ebbaa5c0c14791ef20a975b9c15c65252",
+    "zh:a522991b3d8dfb3b7073c3cd44fb6b8d9957c006ad304e299342e29d11bde854",
+    "zh:b1f931cd16e5a8235131b11c4a0d93f122d22dec597534da1bac03324e564fb1",
+    "zh:fa1f470859b8c5dc778561cbf0749b0c002cfa13bbd3574a2574ea682368d73f",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azuread" {
+  version     = "2.41.0"
+  constraints = "~> 2.30"
+  hashes = [
+    "h1:Hbd4sDaFT/hHmgixKSfWDafB2URvk6hYP91yA3MmsV0=",
+    "zh:1c3e89cf19118fc07d7b04257251fc9897e722c16e0a0df7b07fcd261f8c12e7",
+    "zh:32032e1539da9c986adea46a4fc702b8e3dea287ac825fdbc57ce459e6cfd25b",
+    "zh:37841384780140f7026faf55700087d5e08f504168d67ae7f0d28e7132430c82",
+    "zh:4c272ac7bf8b1aa913eeeca5fcdff25af432c6ae397d963710004b546a068936",
+    "zh:8febf43db3f0bd9d46b2ee31eecf8f3ba912a4553ae049657e0f0c68bdff90a0",
+    "zh:b60a3e92cd2a7af916d70d94d01c67d5bfabf68963f1b5ff75d8a310d66a4522",
+    "zh:b9a8b9554f8ba9d306427aa8b2c0894242286972a61ab33300d6d8ac57bf93ab",
+    "zh:ce7a79964a68a6086fa97e1f38d1c015d24d0732dd7876e179704c383a79f9aa",
+    "zh:e305876a4d44739135264bb11be2c5489903682bb89f7b25561d38be31a9087d",
+    "zh:ec3f63a848b3b2521cda0cf7650c4f85b50cc8905e5065f775d7c855833ab306",
+    "zh:ef2a1ba97f15db88510bbc9b1af611c9bc8bee660d4d8b3b826424e1025487aa",
+    "zh:f40db067d868567e199e16054a554aba5d89c3a19a4264c813dc7212724eeeea",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.60.0"
+  constraints = "~> 3.60.0"
+  hashes = [
+    "h1:TcM/Tp+ngNugh6QXFPO5oFs21RmZUMtsTePkfW7LAew=",
+    "zh:0ee99a2a59b8ec8db077ccbed88739e2c84e5c1dceae166171602e2001e08128",
+    "zh:194636208067e5c24d077aa74e1405a757e70dff35a7618d768a748710be1eba",
+    "zh:1a69482589e31834b38226762263bd0b8fc9f86b16403705d2e4d161e6f209e6",
+    "zh:23fa6b45f2bb19c3218c1b47528e538456444c86b3bb542df02a4ba3b788e662",
+    "zh:715b62b26ee81b07845613e32dc72899c694af91cbc1756b32783cab89836ffe",
+    "zh:8bc1f72454e465c7ac8aaa87bf09dcb5c560fc8d2d976cb11b7f206ff4f2e0d0",
+    "zh:8d4be7c813f3859b78caefcd11e88e9631155986b846bab94a98eadbc25d817e",
+    "zh:aaf63f83ae41f4a3cb6f18d2d5fc403fa2047d5ad74ee2c4aa24db91a3ef851c",
+    "zh:cb444a37d09683340d38629bfb33c925865eebe2ff1ca3f1d0ae470f6a82ab6b",
+    "zh:ed2dae0151e1591799061711e9b63fee283f3e6f47829b2521baab5e04ab5b5e",
+    "zh:f05e8e85a8f61ed1a7d3929fce1a529a9a2aebb2d59704994d4c9e80626aef5d",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version     = "3.5.1"
+  constraints = "~> 3.5.1"
+  hashes = [
+    "h1:0ULxM8/DscMzfiDWg1yclBf/39U44wQmlx745BfYZ80=",
+    "zh:04e3fbd610cb52c1017d282531364b9c53ef72b6bc533acb2a90671957324a64",
+    "zh:119197103301ebaf7efb91df8f0b6e0dd31e6ff943d231af35ee1831c599188d",
+    "zh:4d2b219d09abf3b1bb4df93d399ed156cadd61f44ad3baf5cf2954df2fba0831",
+    "zh:6130bdde527587bbe2dcaa7150363e96dbc5250ea20154176d82bc69df5d4ce3",
+    "zh:6cc326cd4000f724d3086ee05587e7710f032f94fc9af35e96a386a1c6f2214f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:b6d88e1d28cf2dfa24e9fdcc3efc77adcdc1c3c3b5c7ce503a423efbdd6de57b",
+    "zh:ba74c592622ecbcef9dc2a4d81ed321c4e44cddf7da799faa324da9bf52a22b2",
+    "zh:c7c5cde98fe4ef1143bd1b3ec5dc04baf0d4cc3ca2c5c7d40d17c0e9b2076865",
+    "zh:dac4bad52c940cd0dfc27893507c1e92393846b024c5a9db159a93c534a3da03",
+    "zh:de8febe2a2acd9ac454b844a4106ed295ae9520ef54dc8ed2faf29f12716b602",
+    "zh:eab0d0495e7e711cca367f7d4df6e322e6c562fc52151ec931176115b83ed014",
+  ]
+}
--- DevCenter/Catalogs/WebAppWithSQLusingTF/resources.tf
+++ DevCenter/Catalogs/WebAppWithSQLusingTF/resources.tf
@@ -1,3 +1,11 @@
+data "azuread_client_config" "Current" {}
+
+data "azuread_application_published_app_ids" "well_known" {}
+
+data "azuread_service_principal" "MSGraph" {
+  application_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+} 
+
 data "azurerm_resource_group" "Environment" {
   name = "${var.resource_group_name}"
 }
@@ -82,68 +90,115 @@ resource "azurerm_mssql_database" "db" {
   read_scale = false
 }
 
-# # Create a DB Private DNS Zone
-# resource "azurerm_private_dns_zone" "rbest-endpoint-dns-private-zone" {
-#     #name = "${azurerm_private_dns_zone_virtual_network_link.rbest-endpoint-dns-link.name}.database.windows.net"
-#     name = "privatelink.database.windows.net"
-#     resource_group_name = data.azurerm_resource_group.sqlrg.name  
-# }
+# Create a DB Private DNS Zone
+resource "azurerm_private_dns_zone" "rbest-endpoint-dns-private-zone" {
+    #name = "${azurerm_private_dns_zone_virtual_network_link.rbest-endpoint-dns-link.name}.database.windows.net"
+    name = "privatelink.database.windows.net"
+    resource_group_name = data.azurerm_resource_group.Environment.name
+}
 
-# # Link the Private DNS Zone with the VNET
-# resource "azurerm_private_dns_zone_virtual_network_link" "rbest-endpoint-dns-link" {
-#   name = "rbest-vnet"
-#   resource_group_name = data.azurerm_resource_group.sqlrg.name
-#   private_dns_zone_name = azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.name
-#   virtual_network_id = data.azurerm_virtual_network.rbest-vnet.id
-# }
+# Link the Private DNS Zone with the VNET
+resource "azurerm_private_dns_zone_virtual_network_link" "rbest-endpoint-dns-link" {
+  name = "rbest-vnet"
+  resource_group_name = data.azurerm_resource_group.Environment.name
+  private_dns_zone_name = azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.name
+  virtual_network_id = azurerm_virtual_network.primary-vnet.id
+}
 
-# # DB Private Endpoint Connecton
-# data "azurerm_private_endpoint_connection" "rbest-endpoint-connection" {
-#   depends_on = [azurerm_private_endpoint.rbest-db-endpoint]
-#   name = azurerm_private_endpoint.rbest-db-endpoint.name
-#   resource_group_name = data.azurerm_resource_group.sqlrg.name
-# }
+# DB Private Endpoint Connecton
+data "azurerm_private_endpoint_connection" "rbest-endpoint-connection" {
+  depends_on = [azurerm_private_endpoint.rbest-db-endpoint]
+  name = azurerm_private_endpoint.rbest-db-endpoint.name
+  resource_group_name = data.azurerm_resource_group.Environment.name
+}
 
 # Create a DB Private DNS A Record
-# resource "azurerm_private_dns_a_record" "rbest-endpoint-dns-a-record" {
-#   depends_on = [azurerm_mssql_server.rbest-sql-server]
-#   name = lower(azurerm_mssql_server.rbest-sql-server.name)
-#   zone_name = azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.name
-#   resource_group_name = data.azurerm_resource_group.sqlrg.name
-#   ttl = 300
-#   records = [data.azurerm_private_endpoint_connection.rbest-endpoint-connection.private_service_connection.0.private_ip_address]
-# }
+resource "azurerm_private_dns_a_record" "rbest-endpoint-dns-a-record" {
+  depends_on = [azurerm_mssql_managed_instance.primary-sql-server]
+  name = lower(azurerm_mssql_managed_instance.primary-sql-server.name)
+  zone_name = azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.name
+  resource_group_name = data.azurerm_resource_group.Environment.name
+  ttl = 300
+  records = [data.azurerm_private_endpoint_connection.rbest-endpoint-connection.private_service_connection.0.private_ip_address]
+}
 
-# # Create a Private DNS to VNET link
-# resource "azurerm_private_dns_zone_virtual_network_link" "dns-zone-to-vnet-link" {
-#   name = "rbest-sql-db-vnet-link"
-#   resource_group_name = data.azurerm_resource_group.vnetrg.name
-#   private_dns_zone_name = azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.name  
-#   virtual_network_id = data.azurerm_virtual_network.rbest-vnet.id
-# }
+# Create a Private DNS to VNET link
+resource "azurerm_private_dns_zone_virtual_network_link" "dns-zone-to-vnet-link" {
+  name = "rbest-sql-db-vnet-link"
+  resource_group_name = data.azurerm_resource_group.Environment.name
+  private_dns_zone_name = azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.name  
+  virtual_network_id = azurerm_virtual_network.primary-vnet.id
+}
 
 # locals {
 #   admin_password = try(random_password.admin_password[0].result, var.admin_password)
 # }
 
-# Create a DB Private Endpoint
-# resource "azurerm_private_endpoint" "rbest-db-endpoint" {
-#   depends_on = [
-#     azurerm_mssql_server.rbest-sql-server,
-#     azurerm_private_dns_zone.rbest-endpoint-dns-private-zone
-#     ]
-#   name = "rbest-sql-db-endpoint"
-#   location = data.azurerm_resource_group.sqlrg.location
-#   resource_group_name = data.azurerm_resource_group.sqlrg.name
-#   subnet_id = data.azurerm_subnet.rbest-sql-subnet.id
-#   private_service_connection {
-#     name = "rbest-sql-db-endpoint"
-#     is_manual_connection = "false"
-#     private_connection_resource_id = azurerm_mssql_server.rbest-sql-server.id
-#     subresource_names = ["sqlServer"]
-#   }
-#   private_dns_zone_group {
-#     name                 = azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.name
-#     private_dns_zone_ids = [azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.id]
-#   }
-# }
+#Create a DB Private Endpoint
+resource "azurerm_private_endpoint" "rbest-db-endpoint" {
+  depends_on = [
+    azurerm_mssql_managed_instance.primary-sql-server,
+    azurerm_private_dns_zone.rbest-endpoint-dns-private-zone
+    ]
+  name = "rbest-sql-db-endpoint"
+  location = data.azurerm_resource_group.Environment.location
+  resource_group_name = data.azurerm_resource_group.Environment.name
+  subnet_id = azurerm_subnet.sql-subnet.id
+  private_service_connection {
+    name = "rbest-sql-db-endpoint"
+    is_manual_connection = "false"
+    private_connection_resource_id = azurerm_mssql_managed_instance.primary-sql-server.id
+    subresource_names = ["sqlServer"]
+  }
+  private_dns_zone_group {
+    name                 = azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.name
+    private_dns_zone_ids = [azurerm_private_dns_zone.rbest-endpoint-dns-private-zone.id]
+  }
+}
+
+
+resource "azurerm_service_plan" "WebAppDemo" {
+	name                	= "webappdemo${random_integer.ResourceSuffix.result}-plan"
+	location            	= data.azurerm_resource_group.Environment.location
+	resource_group_name 	= data.azurerm_resource_group.Environment.name
+
+	os_type             	= "Windows"
+	sku_name            	= "P1v2"
+}
+
+resource "azurerm_windows_web_app" "WebAppDemoWeb" {
+	name                	= "webappdemo${random_integer.ResourceSuffix.result}-web"
+	location            	= data.azurerm_resource_group.Environment.location
+	resource_group_name 	= data.azurerm_resource_group.Environment.name
+	
+	service_plan_id 		= azurerm_service_plan.WebAppDemo.id
+	https_only 				= true
+
+  site_config {}
+}
+
+resource "azuread_application" "WebAppDemoWebRegistry" {
+	display_name 					= "${data.azurerm_resource_group.Environment.name}-${azurerm_windows_web_app.WebAppDemoWeb.default_hostname}"
+	identifier_uris  				= [ "api://${data.azurerm_resource_group.Environment.name}-${azurerm_windows_web_app.WebAppDemoWeb.default_hostname}" ]
+	owners 							= [ data.azuread_client_config.Current.object_id ]
+	sign_in_audience 				= "AzureADMyOrg"
+
+	web {
+		homepage_url  = "https://${azurerm_windows_web_app.WebAppDemoWeb.default_hostname}"
+		redirect_uris = ["https://${azurerm_windows_web_app.WebAppDemoWeb.default_hostname}/oauth2/callback/aad"]
+	}
+	
+	required_resource_access {
+		resource_app_id = "00000002-0000-0000-c000-000000000000"
+
+		resource_access {
+			id   = "311a71cc-e848-46a1-bdf8-97ff7156d8e6"
+			type = "Scope"
+		}
+
+		resource_access {
+			id   = "3afa6a7d-9b1a-42eb-948e-1650a849e176"
+			type = "Scope"
+		}
+	}
+}
