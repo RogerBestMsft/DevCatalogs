df46936e76c21439be0a17a1f965e84ed2fb41d3
RBDDcet <120424126+RBDDcet@users.noreply.github.com>
2024-01-30 11:33:16 -0800

    Update dummy.ps1

--- DevCenter/Catalogs/Customizations/Tasks/Dummy/dummy.ps1
+++ DevCenter/Catalogs/Customizations/Tasks/Dummy/dummy.ps1
@@ -1,2 +1,2 @@
-Get-Process | Out-File -FilePath "C:\Users\Public\DEFProcess.txt"
-#(New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1') | Out-File -FilePath $Env:PUBLIC\installchoco.ps1
+#Get-Process | Out-File -FilePath "C:\Users\Public\DEFProcess.txt"
+(New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1') | Out-File -FilePath "C:\Users\Public\installchoco.ps1"
