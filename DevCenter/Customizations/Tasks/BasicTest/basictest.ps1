ba38c8037f084320f59364ecbba591babe003c23
RBDDcet <120424126+RBDDcet@users.noreply.github.com>
2024-01-31 15:28:05 -0800

    Update basictest.ps1

--- DevCenter/Customizations/Tasks/BasicTest/basictest.ps1
+++ DevCenter/Customizations/Tasks/BasicTest/basictest.ps1
@@ -1,3 +1,5 @@
 [System.Security.Principal.WindowsIdentity]::GetCurrent().Name  | Out-File -FilePath "C:\Users\Public\CustomA.txt"
 #Get-Process | Out-File -FilePath "C:\Users\Public\DEFProcess.txt"
 Get-WmiObject -Class Win32_Product | Out-File -FilePath "C:\Users\Public\CustomB.txt"
+
+Get-AppxPackage -AllUsers | | Out-File -FilePath "C:\Users\Public\CustomC.txt"
