9db0ac642f7f3e1992cd692592f44aea25a1c18f
RBDDcet <120424126+RBDDcet@users.noreply.github.com>
2024-01-31 13:56:51 -0800

    Update basictest.ps1

--- DevCenter/Customizations/Tasks/BasicTest/basictest.ps1
+++ DevCenter/Customizations/Tasks/BasicTest/basictest.ps1
@@ -1,1 +1,3 @@
-Get-Process | Out-File -FilePath "C:\Users\Public\DEFProcess.txt"
+[System.Security.Principal.WindowsIdentity]::GetCurrent().Name  | Out-File -FilePath "C:\Users\Public\CustomA.txt"
+#Get-Process | Out-File -FilePath "C:\Users\Public\DEFProcess.txt"
+Get-WmiObject -Class Win32_Product | Out-File -FilePath "C:\Users\Public\CustomB.txt"
