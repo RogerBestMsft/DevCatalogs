3c444b9d00e7ff3927316a1b0626fd3018ef102a
Roger <10090643+RogerBestMsft@users.noreply.github.com>
2024-02-15 09:46:15 -0800

    Update CheckToken.sh

--- DevCenter/Catalogs/TestNBAccess/CheckToken.sh
+++ DevCenter/Catalogs/TestNBAccess/CheckToken.sh
@@ -2,4 +2,4 @@
 subscriptionId="572b41e6-5c44-486a-84d2-01d6202774ac"
 
 azureAccessToken=$(az account get-access-token --query accessToken -o tsv)
-curl -sL -H "authorization: bearer $azureAccessToken" -H "content-type: application/json" "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups?api-version=2020-01-01"
+curl -sL POST -H "authorization: bearer $azureAccessToken" -H "content-type: application/json" "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/AlphaAlpha?api-version=2020-01-01"
