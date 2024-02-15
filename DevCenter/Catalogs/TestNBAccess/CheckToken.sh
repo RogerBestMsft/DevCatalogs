382bca8028c7c7e6766799c785e6d1f0e2af45fb
Roger <10090643+RogerBestMsft@users.noreply.github.com>
2024-02-15 10:48:19 -0800

    Send to var and out

--- DevCenter/Catalogs/TestNBAccess/CheckToken.sh
+++ DevCenter/Catalogs/TestNBAccess/CheckToken.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 subscriptionId="572b41e6-5c44-486a-84d2-01d6202774ac"
 
-azureAccessToken=$(az account get-access-token --query accessToken -o tsv)
+TEST=`azureAccessToken=$(az account get-access-token --query accessToken -o tsv)`
 curl -sL -H "authorization: bearer $azureAccessToken" -H "content-type: application/json" "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/AlphaAlpha?api-version=2020-01-01"
+echo "$TEST"
--- DevCenter/Catalogs/TestNBAccess/outputs.tf
+++ DevCenter/Catalogs/TestNBAccess/outputs.tf
@@ -1,7 +1,3 @@
-# output "random_output" {
-#     value = random_integer.ResourceSuffix
-# }
-
 output "checkrg_output" {
     value = null_resource.checktoken
 }
