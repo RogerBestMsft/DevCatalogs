209f5f9f0b5133b9c75d8c2d2fc72c2c41c3fc66
RBDDcet <120424126+RBDDcet@users.noreply.github.com>
2024-02-20 09:49:27 -0800

    Update resources.tf

--- DevCenter/Catalogs/TestNBAccess/resources.tf
+++ DevCenter/Catalogs/TestNBAccess/resources.tf
@@ -2,6 +2,6 @@
 resource "null_resource" "checktoken" {
   provisioner "local-exec" {
     command = "az account get-access-token --query accessToken -o tsv"
-    interpreter = ["bash"]
+    interpreter = ["powershell"]
   }
 }
