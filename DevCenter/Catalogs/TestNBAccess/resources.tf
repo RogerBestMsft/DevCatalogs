bbc60bb008f494aad86663e1780397d1f3df4f6e
RBDDcet <120424126+RBDDcet@users.noreply.github.com>
2024-02-20 09:28:17 -0800

    clean up bash test

--- DevCenter/Catalogs/TestNBAccess/resources.tf
+++ DevCenter/Catalogs/TestNBAccess/resources.tf
@@ -1,22 +1,7 @@
-locals {
-  testvalue = "blue"
-}
-data "azuread_client_config" "Current" {}
-
-data "azuread_application_published_app_ids" "well_known" {}
-
-data "azurerm_resource_group" "Environment" {
-  name = "${var.resource_group_name}"
-}
-
-resource "random_integer" "ResourceSuffix" {
-	min 					= 10000
-	max						= 99999
-}
 
 resource "null_resource" "checktoken" {
   provisioner "local-exec" {
     command = "az account get-access-token --query accessToken -o tsv"
-    interpreter = ["/bin/bash"]
+    interpreter = ["bash"]
   }
 }
