a33aa9616ab048a24aee7c22d35d1b970509b99b
RBDDcet <120424126+RBDDcet@users.noreply.github.com>
2024-02-22 13:36:55 -0800

    Update CheckToken.sh

--- DevCenter/Catalogs/TestNBAccess/CheckToken.sh
+++ DevCenter/Catalogs/TestNBAccess/CheckToken.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 subscriptionId="572b41e6-5c44-486a-84d2-01d6202774ac"
-apt install curl
+sudo apt-get install curl
+
 az login --identity
 az account set --subscription $subscriptionId
 token=$(az account get-access-token --query "accessToken" -otsv)
 BODY='{"location":"eastus"}'
-curl -X PUT -H "Authorization: Bearer $token" -H "Content-Type: application/json" -d "$BODY" https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/CharlieCharlie?api-version=2020-01-01
+/usr/bin/curl -X PUT -H "Authorization: Bearer $token" -H "Content-Type: application/json" -d "$BODY" https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/CharlieCharlie?api-version=2020-01-01
