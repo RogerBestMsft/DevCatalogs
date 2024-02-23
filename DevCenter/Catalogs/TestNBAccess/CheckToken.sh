8898d5263e539708e2c7325e9b048f0264e567a1
RBDDcet <120424126+RBDDcet@users.noreply.github.com>
2024-02-23 15:17:51 -0800

    Update CheckToken.sh

--- DevCenter/Catalogs/TestNBAccess/CheckToken.sh
+++ DevCenter/Catalogs/TestNBAccess/CheckToken.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 az login --identity
-curl -X PUT -H "Authorization: Bearer $(az account get-access-token --query accessToken -o tsv)" -H "Content-Type: application/json" -d '{"location": "eastus"}' https://management.azure.com/subscriptions/572b41e6-5c44-486a-84d2-01d6202774ac/resourceGroups/AAGolf?api-version=2020-01-01
+apt-get install curl
+yum install curl
+zypper install curl
+pacman -Sy curl
+curl -X PUT -H "Authorization: Bearer $(az account get-access-token --subscription "572b41e6-5c44-486a-84d2-01d6202774ac" --query accessToken -o tsv)" -H "Content-Type: application/json" -d '{"location": "eastus"}' https://management.azure.com/subscriptions/572b41e6-5c44-486a-84d2-01d6202774ac/resourceGroups/AAGolf?api-version=2020-01-01
