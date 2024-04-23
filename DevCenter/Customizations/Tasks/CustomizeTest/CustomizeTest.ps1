ae4c5f99d7e544d1fcf0f23458723cfc20efc67f
RBDDcet <120424126+RBDDcet@users.noreply.github.com>
2024-04-23 10:24:34 -0700

    Add customize Test task

---
+++ DevCenter/Customizations/Tasks/CustomizeTest/CustomizeTest.ps1
@@ -0,0 +1,7 @@
+[System.Security.Principal.WindowsIdentity]::GetCurrent().Name  | Out-File -FilePath "C:\Users\Public\CustomA.txt"
+#Get-Process | Out-File -FilePath "C:\Users\Public\DEFProcess.txt"
+Get-WmiObject -Class Win32_Product | Out-File -FilePath "C:\Users\Public\CustomB.txt"
+
+Get-AppxPackage -AllUsers | Out-File -FilePath "C:\Users\Public\CustomC.txt"
+
+Get-Date | Out-File -FilePath "C:\Users\Public\CustomD.txt"
