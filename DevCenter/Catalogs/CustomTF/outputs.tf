9bd2369469417cfb1ef4219641d5687215f4a587
Roger Best <rbest@microsoft.com>
2024-06-28 12:00:16 -0700

    adjust output

--- DevCenter/Catalogs/CustomTF/outputs.tf
+++ DevCenter/Catalogs/CustomTF/outputs.tf
@@ -1,8 +1,3 @@
 output "resource_group_name" {
-  value = data.azurerm_resource_group.EnvironmentRG.name
-}
-
-output "admin_password" {
-  sensitive = true
-  value     = var.admin_password
+  value = data.azurerm_resource_group.rg.name
 }
