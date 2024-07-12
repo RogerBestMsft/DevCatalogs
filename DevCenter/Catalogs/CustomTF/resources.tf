d9c028dd4702a12c90492885556f8b35c8438faa
RBestMSFT <rbest@microsoft.com>
2024-07-12 09:46:34 -0700

    change plan sku

--- DevCenter/Catalogs/CustomTF/resources.tf
+++ DevCenter/Catalogs/CustomTF/resources.tf
@@ -16,7 +16,7 @@ resource "azurerm_service_plan" "appserviceplan" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P3v3"
 }
 
 # Create the web app, pass in the App Service Plan ID
