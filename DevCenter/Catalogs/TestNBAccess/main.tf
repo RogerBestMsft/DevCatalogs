4b0214805a81d6184d3d0ad1dac1e5b874bcb25e
Roger Best <rbest@microsoft.com>
2024-08-15 10:40:42 -0700

    For clean up B

--- DevCenter/Catalogs/TestNBAccess/main.tf
+++ DevCenter/Catalogs/TestNBAccess/main.tf
@@ -1,14 +1,3 @@
-terraform {
-	required_providers {
-		azurerm = {
-			version = "=3.59.0"
-		}
-		azuread = {
-			
-		}
-	}
-}
-
 provider "azurerm" {
 	features {}
 	skip_provider_registration = true
